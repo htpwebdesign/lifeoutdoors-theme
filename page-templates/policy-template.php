4c05f21195c416bd9e1ac86fefb4ae50c39d0a11
Dani Melo <156140180+danielacmelo@users.noreply.github.com>
2024-06-27 23:48:11 -0700

    Removed WooCommerce files from theme folder Moved testimonial.php tp template parts folder Created folder  page-templates and moved policy-template.php Removed code from testimonial php

--- page-templates/policy-template.php
+++ page-templates/policy-template.php
@@ -1,11 +1,8 @@
 <?php
 /**
- * The template for displaying all pages
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
+ * Template Name: Policy Template
+ * 
+ * The template for displaying the policy pages.
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
