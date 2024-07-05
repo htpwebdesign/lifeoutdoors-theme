b9cb54f15d023aef246d76b5a8d5ee2e1282ae2f
Dani Melo <156140180+danielacmelo@users.noreply.github.com>
2024-07-04 17:29:10 -0700

    Update policy template -  function exist wrapper

--- page-templates/policy-template.php
+++ page-templates/policy-template.php
@@ -20,37 +20,38 @@ get_header();
 
         get_template_part( 'template-parts/content', 'page' );
 
-        $policy_overview = get_field('policy_overview');
-        $policy_info = get_field('policy_info');
 
-        if ($policy_overview) :
-            ?>
-            <section class="policy-overview">
-                <h2>Policy Overview</h2>
-                <div class="policy-description">
-                    <?php echo $policy_overview; ?>
-                </div>
-            </section>
-            <?php
-        endif;
+        if ( function_exists( 'get_field' ) ) { 
+            $policy_overview = get_field('policy_overview');
+            $policy_info = get_field('policy_info');
 
-        if ($policy_info && is_array($policy_info)) :
-            ?>
-            <section class="policy-info">
-                <h2>Policy Information</h2>
-                <?php foreach ($policy_info as $info) : ?>
-                    <?php if (isset($info['subtitle']) && isset($info['description'])) : ?>
-                        <div class="policy-item">
-                            <h3><?php echo esc_html($info['subtitle']); ?></h3>
-                            <div class="policy-description">
-                                <?php echo $info['description']; ?>
+            if ($policy_overview) :
+                ?>
+                <section class="policy-overview">
+                    <div class="policy-description">
+                        <?php echo $policy_overview; ?>
+                    </div>
+                </section>
+                <?php
+            endif;
+
+            if ($policy_info && is_array($policy_info)) :
+                ?>
+                <section class="policy-info">
+                    <?php foreach ($policy_info as $info) : ?>
+                        <?php if (isset($info['subtitle']) && isset($info['description'])) : ?>
+                            <div class="policy-item">
+                                <h2><?php echo esc_html($info['subtitle']); ?></h2>
+                                <div class="policy-description">
+                                    <?php echo $info['description']; ?>
+                                </div>
                             </div>
-                        </div>
-                    <?php endif; ?>
-                <?php endforeach; ?>
-            </section>
-            <?php
-        endif;
+                        <?php endif; ?>
+                    <?php endforeach; ?>
+                </section>
+                <?php
+            endif;
+        }            
 
     endwhile; // End of the loop.
     ?>
