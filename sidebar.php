6c596f54ff2ef3c4b6fc37241f0e632aa9aa20c9
Dani Melo <156140180+danielacmelo@users.noreply.github.com>
2024-07-18 02:21:52 -0700

    Registered new sidebar and add conditional

--- sidebar.php
+++ sidebar.php
@@ -7,11 +7,19 @@
  * @package Life_Outdoors
  */
 
-if ( ! is_active_sidebar( 'sidebar-1' ) ) {
+ if ( ! is_active_sidebar( 'sidebar-1' )  || ! is_active_sidebar( 'sidebar-2' ) ) {
 	return;
 }
 ?>
 
-<aside id="secondary" class="widget-area">
-	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+<?php
+if ( is_product_category() ) {
+    // For  taxonomy-product-cat.php template Product category page
+    dynamic_sidebar( 'sidebar-1' ); // sidebar-1 for product category
+} elseif ( is_post_type_archive( 'product' ) ) {
+    // For archive-product.php template Shop page
+    dynamic_sidebar( 'sidebar-2' ); 
+} 
+?>
+
 </aside><!-- #secondary -->
