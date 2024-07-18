eb9ba45567fa662df7921ee9240fcb04829c01de
Dani Melo <156140180+danielacmelo@users.noreply.github.com>
2024-07-18 12:05:26 -0700

    Add title icon search

--- header.php
+++ header.php
@@ -72,7 +72,7 @@
                         <span class="screen-reader-text"><?php echo _x( 'Search for:', 'label' ) ?></span>
                         <input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search...', 'placeholder' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
                     </label>
-                    <button type="submit" class="search-submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+                    <button type="submit" class="search-submit"><i class="fa-solid fa-magnifying-glass" title="Search"></i></button>
                 </form>
             </div>
         </nav><!-- #site-navigation -->
